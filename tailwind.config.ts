81a303dad69894ac985ca646d04c9df91e8fdbae
ftmhabt <ftmh.a78@gmail.com>
2024-10-06 17:36:04 +0330

    Fix divider style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        divider: "url('/wavy-steps.svg')",
+        divider: "url('/waves-2-large.svg')",
       },
     },
   },
