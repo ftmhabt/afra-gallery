a6e11be65ff108cdb38a9af8697807a7dfe479e7
ftmhabt <ftmh.a78@gmail.com>
2024-11-08 19:45:07 +0330

    Fix prop typing name

--- src/app/_ui/card.tsx
+++ src/app/_ui/card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Card({ imageSource }: { imageSorce: string }) {
+export default function Card({ imageSource }: { imageSource: string }) {
   return (
     <div className="rounded-xl bg-white flex gap-4 p-4 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
       <Image
